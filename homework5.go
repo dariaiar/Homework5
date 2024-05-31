0d90664b09b4755ce59ccc913368005ff8a9a723
Daria Iaroshchuk <daria.iaroshchuk@gmail.com>
2024-05-31 15:23:43 +0300

    v2

--- homework5.go
+++ homework5.go
@@ -7,49 +7,39 @@ import (
 // чи можна виковистовувати методи для мапів окремо, не вкладаючи їх в структуру?
 func main() {
 
-	var spaceObjects SpaceObjects
+	var week1 Schedule
+	week1.initMap()
+	//week1.printMap()
 
-	spaceObjects.inputMap()
-	spaceObjects.printMap()
+	fmt.Printf("To find daily schedule, please, input day of the week: ")
+	var findDaySchedule string
+	fmt.Scan(&findDaySchedule)
+	week1.Search(findDaySchedule)
 
-	fmt.Printf("Input Key: ")
-	var findObject string
-	fmt.Scan(&findObject)
-
-	spaceObjects.Search(findObject)
 }
 
-type SpaceObjects struct {
-	objects map[string]string
+type Schedule struct {
+	week1Schedule map[string]string
 }
 
-func (s *SpaceObjects) inputMap() {
-	s.objects = map[string]string{
-		"Sun":   "star",
-		"Mars":  "planet",
-		"Moon":  "satellite",
-		"Earth": "planet",
+func (s *Schedule) initMap() map[string]string {
+	s.week1Schedule = map[string]string{
+		"Monday":    "\n1.English;\n2.Mathematics;\n3.Music;\n4.Tennis;",
+		"Tuesday":   "\n1.Ckooking;\n2.Sport;\n3.Drawing",
+		"Wednesday": "\n1.Mathematics;\n2.Tennis;\n3.English",
+		"Thursday":  "\n1.Literature;\n2.Sport;\n3.Reading;\n4.Building",
+		"Friday":    "\n1.English;\n2.Sport;",
 	}
+	return s.week1Schedule
 }
 
-func (s *SpaceObjects) printMap() {
-	fmt.Println("All elements of map", s.objects)
+func (s *Schedule) printMap() {
+	fmt.Println("All elements", s.week1Schedule)
 }
 
-// Example:
-//var people = map[string]int{
-//    "Tom": 1,
-//    "Bob": 2,
-//    "Sam": 4,
-//    "Alice": 8,
-// }
-//if val, ok := people["Tom"]; ok{
-//    fmt.Println(val)
-//}
-
-func (s *SpaceObjects) Search(key string) {
-	if why, yes := s.objects[key]; yes {
-		fmt.Printf("%v is a %v\n", key, why)
+func (s *Schedule) Search(key string) {
+	if val, ok := s.week1Schedule[key]; ok {
+		fmt.Printf("%v schedule is a %v\n", key, val)
 	} else {
 		fmt.Printf("No such key exists. Try again.\n")
 	}
