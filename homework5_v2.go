6c691c749a79ec12f3460e99e5443825ca6f0db6
Daria Iaroshchuk <daria.iaroshchuk@gmail.com>
2024-06-05 10:01:28 +0300

    del comments

--- homework5_v2.go
+++ homework5_v2.go
@@ -32,40 +32,6 @@ func getTextFromFile() []string {
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading from file:", err)
 	}
-	//fmt.Println("Your text is:  ", text)
-	//for b, rangedText := range text {
-	//	fmt.Println(b, rangedText)
-	//}
+
 	return text
 }
-
-//func newIndex(s index.searchByIndex) (text []string, indexText map[string][]int) {
-//	index.searchByIndex(text)(indexText)
-//	index.Search(indexText)
-//	return
-//}
-
-//func insertDataToMap(text []string) map[string][]int {
-//	indexText := make(map[string][]int)
-//	for i, line := range text {
-//		words := strings.Fields(line)
-//		for _, word := range words {
-//			word = strings.ToLower(word)
-//			indexText[word] = append(indexText[word], i)
-//
-//		}
-//	}
-//	return indexText
-//}
-
-//func search(indexText map[string][]int) {
-//	var inputSearchKey string
-//	fmt.Println("Input word to search")
-//	fmt.Scan(&inputSearchKey)
-//	inputSearchKey = strings.ToLower(inputSearchKey)
-//	if val, ok := indexText[inputSearchKey]; ok {
-//		fmt.Printf("The word '%v' exists in lines# %v\n", inputSearchKey, val)
-//	} else {
-//		fmt.Printf("No such key exists. Try again.\n")
-//	}
-//}
